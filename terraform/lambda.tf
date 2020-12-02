78e4fae3693442e580e9a6a6ee3f1f2afae7c69c
davidjoliver86 <davidjoliver86@gmail.com>
2020-12-02 06:21:28 +0000

    fix id to arn, another gitignore

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "newegg_stock_checker" {
   function_name    = var.function_name
   filename         = var.function_zipfile_name
   source_code_hash = filebase64sha256(var.function_zipfile_name)
-  role             = aws_iam_role.newegg_stock_checker.id
+  role             = aws_iam_role.newegg_stock_checker.arn
   handler          = "newegg.lambda_handler"
   runtime          = "python3.8"
 }
